867833a60ba16c16f4c736560eff882b6f977ad9
jasoncao11 <tiekuncao@163.com>
2021-08-16 14:03:49 +0800

    Update script.py

--- Tutorial-2. Mouse event/script.py
+++ Tutorial-2. Mouse event/script.py
@@ -4,25 +4,30 @@ import numpy as np
 
 def click_event(event, x, y, flags, param):
     #显示坐标
+    print(x, y)
     if event == cv2.EVENT_LBUTTONDOWN:
         font = cv2.FONT_HERSHEY_SIMPLEX
         strxy = str(x) + ', ' + str(y)
-        cv2.putText(img, strxy, (x,y), font, 0.5, (255,255,0), 2)
+        cv2.putText(img, strxy, (x, y), font, 0.5, (255,255,0), 2)
         cv2.imshow('image', img)
     #显示BGR每个channel大小
     if event == cv2.EVENT_RBUTTONDOWN:
-        blue = img[x, y, 0] 
-        green = img[x, y, 1]
-        red = img[x, y, 2]
+        #print(y, x)
+        blue = img[y, x, 0]
+        #print(blue)
+        green = img[y, x,  1]
+        #print(green)
+        red = img[y, x,  2]
+        #print(red)
         font = cv2.FONT_HERSHEY_SIMPLEX
         strbgr = str(blue) + ', ' + str(green) + ', ' + str(red)
         cv2.putText(img, strbgr, (x,y), font, 0.5, (0,255,255), 2)
         cv2.imshow('image', img)
     #显示点击处的颜色
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        blue = img[x, y, 0] 
-        green = img[x, y, 1]
-        red = img[x, y, 2]        
+        blue = img[y, x, 0] 
+        green = img[y, x, 1]
+        red = img[y, x, 2]        
         mycolorimage = np.zeros((512, 512, 3), np.uint8)
         mycolorimage[:] = [blue, green, red]
         cv2.imshow('color', mycolorimage)
@@ -35,6 +40,7 @@ def click_event(event, x, y, flags, param):
         cv2.imshow('image', img)
         
 img = cv2.imread('../data/messi5.jpg')
+print(img.shape)
 cv2.imshow('image', img)
 points = []
 cv2.setMouseCallback('image', click_event)
